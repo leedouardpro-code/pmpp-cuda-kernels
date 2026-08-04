992117efa3b7305547c0e4d53cd3a9b56edf5728
leedouardpro-code <le.edouardpro@gmail.com>
2026-08-04 14:39:42 +0200

    + ch5 visual test

--- ch5/matmul_tiling/test_matmul.cu
+++ ch5/matmul_tiling/test_matmul.cu
@@ -6,7 +6,7 @@
 #include "matmul.cuh"
 #define TILE_WIDTH 32
 
-static int check(int width, int height, const float tolerence) {
+static int check(int width, int height, const float tolerence, int visual = 0) {
     const size_t size = (size_t)width * height * sizeof(float);
 
     // Host memory allocation
@@ -29,10 +29,29 @@ static int check(int width, int height, const float tolerence) {
     matmul_cpu(A_h, B_h, C_h_cpu, width, height);
 
     // Verification
+    if (visual){
+        printf("\n===== GPU Visual =====\n")
+        for (int row = 0; row < height; row++) {
+            printf("|")
+            for (int col = 0; col < width; col++) {
+                printf("%f|",C_h_gpu[row*width+col])
+            }
+            printf("\n")
+        }
+        printf("\n===== CPU Visual =====\n")
+        for (int row = 0; row < height; row++) {
+            printf("|")
+            for (int col = 0; col < width; col++) {
+                printf("%f|",C_h_cpu[row*width+col])
+            }
+            printf("\n")
+        }
+    }
+
     for (int i = 0; i < width * height; i++) {
         if (fabsf(C_h_gpu[i] - C_h_cpu[i]) > tolerence) {
             printf("[FAIL] test : width=%d, height=%d\n at index=%d:\n gpu:%f\n cpu:%f\n", width,
-                   height, i, C_h_gpu[i], C_h_cpu[i]);
+                height, i, C_h_gpu[i], C_h_cpu[i]);
             free(A_h);
             free(B_h);
             free(C_h_gpu);
@@ -42,6 +61,7 @@ static int check(int width, int height, const float tolerence) {
     }
     printf("[PASS] test : width=%d, height=%d\n", width, height);
 
+
     // Free Host memory
     free(A_h);
     free(B_h);
@@ -49,6 +69,8 @@ static int check(int width, int height, const float tolerence) {
     free(C_h_cpu);
     return 0;
 }
+
+static int visual_check
 
 int main(void) {
     int width[] = {1, 33, 2 << 7, 2 << 9};
@@ -62,5 +84,9 @@ int main(void) {
                   1e-4);  // tolerance is ajusted because cpu and gpu dont round off the same way
     }
 
+    // visual test
+    int w = 33;
+    int h = 33;
+    check(width[i], height[i], 1e-4, 1);
     return failures ? EXIT_FAILURE : EXIT_SUCCESS;
 }
